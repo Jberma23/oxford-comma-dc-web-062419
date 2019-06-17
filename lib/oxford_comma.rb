012c1f4c2984b7194deeee3f478b7f711f6e5a8a
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-17 11:31:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-array.join(", ")
-if array.length >2
-  Array(-2) << " , and "
+if array.length == 1
+array.join
+elsif array.length == 2
+  array.join(" and ")
+else
+  array[0..-2].join(", ") << ", and" << array[-1]
 end
 end
